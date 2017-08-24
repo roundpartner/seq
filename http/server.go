c0de5e57356c2c86fa3165f2c09916607c31194b
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-08-24 22:11:03 +0100

    Implement Get and Post messages

--- http/server.go
+++ http/server.go
@@ -5,6 +5,8 @@ import (
     "net/http"
     "fmt"
     "github.com/roundpartner/seq/buffer"
+    "io/ioutil"
+    "encoding/json"
 )
 
 var buf chan buffer.Message = nil
@@ -19,15 +21,31 @@ func Serve() {
 }
 
 func Get(w http.ResponseWriter, req *http.Request) {
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprint(w, "{}")
+    message, ok := buffer.Pop(buf)
+    if false == ok {
+        w.Header().Set("Content-Type", "application/json; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        fmt.Fprint(w, "{}")
+        return
+    }
+    js, err := json.Marshal(message)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(js)
 }
 
 func Post(w http.ResponseWriter, req *http.Request) {
     w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
-    res := buffer.Add(buf, "Hello")
+    body, error := ioutil.ReadAll(req.Body)
+    if error != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    res := buffer.Add(buf, string(body))
     if false == res {
         w.WriteHeader(http.StatusInternalServerError)
         return
--- http/server_test.go
+++ http/server_test.go
@@ -5,6 +5,7 @@ import (
     "net/http"
     "net/http/httptest"
     "github.com/roundpartner/seq/buffer"
+    "strings"
 )
 
 func TestGet(t *testing.T) {
@@ -28,6 +29,16 @@ func TestGetReturnsEmptyJson(t *testing.T) {
     }
 }
 
+func TestGetReturnsMessage(t *testing.T) {
+    buf = buffer.Create(1)
+    buffer.Add(buf, "Hello World")
+    rr := recordGet(t)
+    if "\"Hello World\"" != rr.Body.String() {
+        t.Errorf("response: %s", rr.Body.String())
+        t.Fail()
+    }
+}
+
 func recordGet(t *testing.T) *httptest.ResponseRecorder {
     rr := httptest.NewRecorder()
     req, err := http.NewRequest("GET", "/", nil)
@@ -39,16 +50,33 @@ func recordGet(t *testing.T) *httptest.ResponseRecorder {
 }
 
 func TestPost(t *testing.T) {
-    rr := recordPost(t)
+    rr := recordPost(t, "")
     if rr.Code != http.StatusNoContent {
         t.Fail()
     }
 }
 
-func recordPost(t *testing.T) *httptest.ResponseRecorder {
+func TestPostAddsToBuffer(t *testing.T) {
+    recordPost(t, "")
+    _, ok := buffer.Pop(buf)
+    if false == ok {
+        t.Fail()
+    }
+}
+
+func TestPostAddsMessageToBuffer(t *testing.T) {
+    recordPost(t, "Hello World")
+    message, _ := buffer.Pop(buf)
+    if "Hello World" != message {
+        t.Fail()
+    }
+}
+
+func recordPost(t *testing.T, body string) *httptest.ResponseRecorder {
     buf = buffer.Create(1)
     rr := httptest.NewRecorder()
-    req, err := http.NewRequest("POST", "/", nil)
+    r := strings.NewReader(body)
+    req, err := http.NewRequest("POST", "/", r)
     if err != nil {
         t.Fatal(err)
     }
