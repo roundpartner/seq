70fce7f094f276a62c2c73338c4063098d242f96
Tom <tom@thomaslorentsen.co.uk>
2017-08-26 19:12:17 +0100

    Add buffer to Post and Get
    Implement Get and Post messages

--- http/server.go
+++ http/server.go
@@ -4,9 +4,16 @@ import (
     "github.com/gorilla/mux"
     "net/http"
     "fmt"
+    "github.com/roundpartner/seq/buffer"
+    "io/ioutil"
+    "encoding/json"
 )
 
+var buf chan buffer.Message = nil
+
 func Serve() {
+    buf = buffer.Create(1)
+
     router := mux.NewRouter()
     router.HandleFunc("/", Get).Methods("GET")
     router.HandleFunc("/", Post).Methods("POST")
@@ -14,12 +21,34 @@ func Serve() {
 }
 
 func Get(w http.ResponseWriter, req *http.Request) {
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprint(w, "{}")
+    message, ok := buffer.Pop(buf)
+    if false == ok {
+        w.Header().Set("Content-Type", "application/json; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        fmt.Fprint(w, "{}")
+        return
+    }
+    js, err := json.Marshal(message)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(js)
 }
 
 func Post(w http.ResponseWriter, req *http.Request) {
-    w.WriteHeader(http.StatusNoContent)
     w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+    body, error := ioutil.ReadAll(req.Body)
+    if error != nil {
+        w.WriteHeader(http.StatusBadRequest)
+        return
+    }
+    res := buffer.Add(buf, string(body))
+    if false == res {
+        w.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+    w.WriteHeader(http.StatusNoContent)
 }
