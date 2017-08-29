bb2a06937dd3e36e81fc391d535abcd6a74ed392
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-08-29 09:08:18 +0100

    Add infinite buffer

--- buffer/buffer.go
+++ buffer/buffer.go
@@ -6,11 +6,35 @@ type Message struct {
 
 type SimpleBuffer struct {
     Messages chan Message
+    In chan Message
+    Out chan Message
+    buffer[] Message
 }
 
 func New(size int) *SimpleBuffer {
-    return &SimpleBuffer{
+    sb := &SimpleBuffer{
         Messages: make(chan Message, size),
+        In: make(chan Message),
+        Out: make(chan Message),
+        buffer: make([]Message, 0),
+    }
+    go sb.run()
+    return sb
+}
+
+func (sb *SimpleBuffer) run() {
+    for {
+        if len(sb.buffer) > 0 {
+            select {
+                case sb.Out<- sb.buffer[0]:
+                    sb.buffer = sb.buffer[1:]
+                case value := <-sb.In:
+                    sb.buffer = append(sb.buffer, value)
+            }
+        } else {
+            value := <-sb.In
+            sb.buffer = append(sb.buffer, value)
+        }
     }
 }
 
